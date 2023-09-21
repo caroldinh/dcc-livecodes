ae327dca2642097a26f17143c94d1bed2ab56764
Reza Amanahu <70492029+rezaamanahu@users.noreply.github.com>
2023-09-21 17:53:46 -0400

    Create hydra.js

---
+++ RAA/hydra.js
@@ -0,0 +1,8 @@
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+// by Débora Falleiros Gonzales
+// https://www.gonzalesdebora.com/
+osc(7)
+	.add(noise(3, 1))
+	.color(1, 2, 3)
+	.colorama(0.5)
+	.out();
